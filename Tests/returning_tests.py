e8788b5179a3f5f21332aae142dbe0dd59c57da0
ba-2022 <112644336+ba-2022@users.noreply.github.com>
2022-10-09 11:23:01 -0400

    testing the addition of two numbers and returning the value

--- Tests/returning_tests.py
+++ Tests/returning_tests.py
@@ -6,15 +6,17 @@ class MyTestCase(unittest.TestCase):
 
     def test_return_addtion(self):
 
-        self.assertEqual(15, return_addtion.add_two(7, 8))
-        self.assertEqual(45, return_addtion.add_two(40, 5))
+        self.assertEqual(15, return_addtion.addTwo(7, 8))
+        self.assertEqual(45, return_addtion.addTwo(40, 5))
         # Add two more tests of your own below here
-        self.assertEqual(5, return_addtion.total_value(2, 3))
-        self.assertEqual(20, return_addtion.total_value(12, 8))
+        self.assertEqual(-1, return_addtion.addTwo(2, -3))
+        self.assertEqual(-20, return_addtion.addTwo(-12, -8))
 
+'''
     def test_triangle_area(self):
         self.assertEqual(6.0, triangle_area.area(3, 4, 5))
 
 
 if __name__ == '__main__':
     unittest.main()
+'''
