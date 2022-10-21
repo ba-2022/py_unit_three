e9411e85cee2021e6294b79505c8f53268837948
ba-2022 <112644336+ba-2022@users.noreply.github.com>
2022-10-21 10:12:04 -0400

    finale project 3

--- assignment_three.py
+++ assignment_three.py
@@ -1,61 +1,54 @@
 # Betelhem Alemu
-# 10/18/22
+# 10/20/22
 # This program creates a hexagon flower
 
 import turtle
 
-''' This function asks the user for a side length'''
-get_side_length = int(input("Please enter a side length"))
+# This function asks the user for a side length
+def get_side_length():
+    return float(input("Enter a side length:"))
 
+# This functions asks the user for a center color
+def get_center_color():
+    return str(input("Enter the color of the center:"))
 
-''' This function asks the user for a color for the center of the flower'''
-get_center_color = str(input("Please enter a color for the center"))
+# This functions asks the user for the petal colors
+def get_petal_color():
+    return str(input("Enter the color of the petals:"))
 
-''' This function asks the user for the flowers petal color'''
-get_petal_color = input("Please enter a color for the petal")
+''' This function creates the flower
+'''
+def draw_a_flower(size, center, petal,):
 
-movement = get_side_length * -2
+    draw_a_hexagon(size,center)
+    turtle.forward(size)
+    turtle.left(180)
+    for x in range(6):
+        draw_a_hexagon(size, petal)
+        turtle.forward(size)
+        turtle.left(60)
 
-# this function calls for the drawing of the hehxagon
-def draw_flower(color):
-        turtle.fillcolor(color)
-        turtle.begin_fill()
-        for x in range(6):
-            turtle.forward(get_side_length)
-            turtle.pendown()
-            turtle.left(60)
-        turtle.end_fill()
+'''This functions creates the hexagon, which is the shape of the flower being drawn
+'''
+def draw_a_hexagon(size, color):
+    turtle.begin_fill()
+    turtle.fillcolor(color)
+    for x in range(6):
+        turtle.forward(size)
+        turtle.left(300)
+    turtle.end_fill()
 
-# this function draws the center of the flower and fills in the color inputed by the user
-def draw_center(color):
-    turtle.right(60)
-    turtle.left(60)
-    turtle.forward(movement)
-    draw_flower(get_center_color)
-
-
-# this function draws the petals of the flower and fills in the color inputed by the user
-def draw_petal():
-    for x in range(5):
-        turtle.forward(get_side_length)
-        turtle.right(60)
-        draw_flower(get_petal_color)
-
-
+'''This function calls the other functions
+'''
 def main():
-    draw_flower(get_petal_color)
-    draw_petal()
-    draw_center(get_center_color)
+    sideLength = get_side_length()
+    centerColor = get_center_color()
+    petalColor = get_petal_color()
 
-
-
-
+    draw_a_flower(sideLength, centerColor, petalColor)
 
 main()
 
-'''
-after running the code, this function lets the screen stay open
-until you click on exit
-'''
+
 
 turtle.exitonclick()
